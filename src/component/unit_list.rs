62a84025fdbb58680eac82b497b69e72fcabf2c6
Jason Youngberg <jasondyoungberg@gmail.com>
2024-01-18 12:28:05 -0700

    Refactor UnitList component to use autoprops

--- src/component/unit_list.rs
+++ src/component/unit_list.rs
@@ -1,53 +1,21 @@
-use crate::*;
+use crate::UnitConfig;
+use crate::Unit;
+use crate::Msg;
 
-#[derive(Properties, PartialEq)]
-pub struct UnitListProps {
-    pub units: Vec<Unit>,
-    pub messenger: Callback<Msg>
-}
+use yew::prelude::*;
+use yew_autoprops::autoprops;
 
-pub struct UnitList { }
-
-impl Component for UnitList {
-    type Message = Msg;
-    type Properties = UnitListProps;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        UnitList { }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let props = ctx.props();
-        let messenger = &props.messenger;
-
-        props.units.iter().enumerate().map(|(i, unit)| {
-            html!{
-                <UnitConfig
-                    unit={unit.clone()}
-                    id={i}
-                    messenger={messenger.clone()}
-                />
-            }
-        }).collect::<Html>()
-    }
-}
-
-/*
+#[autoprops]
 #[function_component(UnitList)]
-pub fn unit_list(UnitListProps { units, on_update }: &UnitListProps) -> Html {
+pub fn unit_list(
+    units: &Vec<Unit>,
+    messenger: Callback<Msg>
+) -> Html {
     units.iter().enumerate().map(|(i, unit)| {
-        let update_handler = {
-            let on_update = on_update.clone();
-            Callback::from(move |msg| {
-                on_update.emit(msg);
-            })
-        };
-
-        html!{
-            <>
-                <UnitConfig unit={unit.clone()} id={i} on_update={update_handler}/>
-            </>
+        html!{<UnitConfig 
+            unit={unit.clone()}
+            id={i}
+            messenger={messenger.clone()}/>
         }
     }).collect::<Html>()
 }
-*/
